d316b82bd55c5858d17cc88c07b4a474f8bc00fb
s4ntifdz <santiago.fernandez.utn@gmail.com>
2023-08-19 02:21:10 -0300

    Funciona el modulo stock, falta arreglar el 'save' a la hora de guardar los datos

--- app/stock/models.py
+++ app/stock/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
 #Duda:
-from app.clients.models import ClientModel
+from clients.models import ClientModel
 # from django.contrib.auth.models import AbstractUser ?
 
 #otra duda, esta app solo se "activaria" cuando el is_seller = True, como y cuando se valida eso?
@@ -14,12 +14,15 @@ class BusinessLineChoices(models.TextChoices):
     OTROS = "Otros"
     
 class stock_product(models.Model):
-    user = models.ForeignKey(ClientModel)
+    user = models.ForeignKey(ClientModel,on_delete=models.CASCADE) #si se elimina un cliente, se eliminan los productos cargados por el
     business_line = models.CharField(choices=BusinessLineChoices.choices, max_length=128, default=BusinessLineChoices.OTROS.value)
-    quantity = models.IntegerField(max_length=10)
-    price = models.FloatField(max_length=10)
+    quantity = models.PositiveIntegerField() 
+    price = models.DecimalField(max_digits=10, decimal_places=2)  
     name_product = models.CharField(max_length=140)
     description = models.TextField(max_length=1200)
-    code_product = models.IntegerField(max_length=10)
+    code_product = models.CharField(max_length=10)
     # picture = 
     expiration = models.DateTimeField()    
+    
+    def __str__(self):
+        return self.name_product
