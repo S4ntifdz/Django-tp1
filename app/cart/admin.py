33afe861e7dd9832a9538d29a3c0b0e87348302c
s4ntifdz <santiago.fernandez.utn@gmail.com>
2023-08-21 04:12:18 -0300

    No se modifica el carrito padre al cambiar de user CORREGIR

--- app/cart/admin.py
+++ app/cart/admin.py
@@ -1,7 +1,25 @@
 from django.contrib import admin
 from cart.models import CartModel
+from cart.models import Cart_fath
 # Register your models here.
 
 @admin.register(CartModel)
 class CartModel (admin.ModelAdmin):
     pass
+# @admin.register(Cart_fath)
+# class Cart_fath (admin.ModelAdmin):
+#     pass
+
+    
+from django.contrib import admin
+from .models import Cart_fath
+
+@admin.register(Cart_fath)
+class Cart_fathAdmin(admin.ModelAdmin):
+    list_display = ('user',)
+
+    def change_view(self, request, object_id, form_url='', extra_context=None):
+        # Limpia el carrito actual antes de cambiar al nuevo usuario.
+        cart_instance = Cart_fath.objects.get(pk=object_id)
+        cart_instance.items.clear()
+        return super().change_view(request, object_id, form_url, extra_context)
--- app/cart/models.py
+++ app/cart/models.py
@@ -1,14 +1,46 @@
+# from django.db import models
+# from clients.models import ClientModel
+# from stock.models import stock_product
+# # Create your models here.
+# # DUDA: Para la cantidad del carrito de compras, tengo que extender la clase productos?
+# # si ese es el caso, a que clase conecto como fk
+
+# # validaciones en donde
+
+# # class product_with_qt(stock_product, models.Model):
+# #     name = stock_product.name_product
+# #     quantity = models.IntegerField()
+
+# class CartModel (models.Model):
+#    # user = models.ForeignKey(ClientModel,on_delete=models.CASCADE)
+#     products = models.ForeignKey(stock_product,on_delete=models.CASCADE)
+#     quantity = models.IntegerField(default=1)
+    
+#     def __str__(self): 
+#         return f"{self.products} | cantidad: {self.quantity}"
+
+# class Cart_fath(models.Model):
+#     super(CartModel)
+#     user = models.ForeignKey(ClientModel,on_delete=models.CASCADE)
+
 from django.db import models
 from clients.models import ClientModel
 from stock.models import stock_product
-# Create your models here.
-# DUDA: Para la cantidad del carrito de compras, tengo que extender la clase productos?
-# si ese es el caso, a que clase conecto como fk
 
-class product_with_qt(stock_product, models.Model):
-    name_product 
-    quantity = models.IntegerField()
+class CartModel(models.Model):
+    #user = models.ForeignKey(ClientModel, on_delete=models.CASCADE)
+    products = models.ForeignKey(stock_product, on_delete=models.CASCADE)
+    quantity = models.PositiveIntegerField(default=1)
 
-class CartModel (models.Model):
-    user = models.ForeignKey(ClientModel,on_delete=models.CASCADE)
-    products = models.ForeignKey(stock_product,on_delete=models.CASCADE)
+    def __str__(self):
+        return f"{self.products} | Cantidad: {self.quantity}"
+
+class Cart_fath(models.Model):
+    user = models.OneToOneField(ClientModel, on_delete=models.CASCADE)  # Cambiamos a OneToOneField para garantizar que cada usuario tenga un solo carrito.
+    items = models.ManyToManyField(CartModel)
+
+    def __str__(self):
+        return f"Carrito de {self.user}"
+
+    
+
